9e16be434018af703152ce6801041aacba43a695
avi <a0548589558@gmail.com>
2025-08-21 14:40:24 +0300

    added commands to commands.bat

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,2 +1,7 @@
 docker build -t avigoldshtein/hostile-tweets-ex-app .
 docker push avigoldshtein/hostile-tweets-ex-app:latest
+
+oc apply -f secrets.yaml
+oc apply -f deployment.yaml
+oc apply -f service.yaml
+oc apply -f route.yaml
